ce19aac44a4f5c14ed093f43dadd72be91528113
See Boon Soo <sooseeboon@gmail.com>
2020-05-06 15:43:43 -0400

    Add hystrix dashboard

--- TennisPortalService/src/main/java/com/tennisportal/TennisPortalApplication.java
+++ TennisPortalService/src/main/java/com/tennisportal/TennisPortalApplication.java
@@ -3,10 +3,12 @@ package com.tennisportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableHystrixDashboard
 @EnableHystrix
 @Configuration
 @EnableSwagger2
